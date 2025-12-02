219eae56d86bf9746f75dad1b124b91687290097
Kunal Thalautiya <152262114+Kunalkthalautiya@users.noreply.github.com>
2025-12-02 15:59:07 +0530

    Delete src/api directory

--- src/api/posts.ts
+++
@@ -1,9 +0,0 @@
-import api from "./axios";
-
-export const createPost = async (data: FormData) => {
-  return api.post("/posts", data);
-};
-
-export const getFeedPosts = async (page = 1) => {
-  return api.get(`/posts/feed?page=${page}`);
-};
